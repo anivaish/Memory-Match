d4df85e811764d302848c9c71a044febce8f11a6
Aniruddh Vaish <aniruddhvaish2024@gmail.com>
2023-04-03 04:40:17 +0530

    first

---
+++ index.js
@@ -0,0 +1,146 @@
+let cardsArray = [
+    {
+        'id': '1',
+        'name': 'iron',
+        'img': './img/iron.jpg',
+    },
+    {
+        'id': '2',
+        'name': 'cap',
+        'img': './img/cap.jpg',
+    },
+    {
+        'id': '3',
+        'name': 'spider',
+        'img': './img/spider.jpg',
+    },
+    {
+        'id': '4',
+        'name': 'avenger',
+        'img': './img/avengers.jpg',
+    },
+    {
+        'id': '5',
+        'name': 'panther',
+        'img': './img/panther.jpg',
+    },
+    {
+        'id': '6',
+        'name': 'spiderman',
+        'img': './img/spiderman.jpg',
+    },
+    {
+        'id': '7',
+        'name': 'bat',
+        'img': './img/bat.jpg',
+    },
+    {
+        'id': '8',
+        'name': 'super',
+        'img': './img/super.jpg',
+    }
+];
+const len = cardsArray.length;
+for (let i = 0; i < len; i++) {
+    let obj = {
+        'id': `${i + 9}`,
+        'name': `${cardsArray[i].name}`,
+        'img': `${cardsArray[i].img}`
+    }
+    cardsArray.push(obj);
+}
+let shuffledChild = Array.from(cardsArray).sort(() => 0.5 - Math.random());
+
+let clickCount = 0;
+let firstCard = "";
+let firstCard_id = "";
+let secondCard = "";
+
+const card_matches = () => {
+    let card_selected = document.querySelectorAll('.card_selected');
+
+    card_selected.forEach((curElem) => {
+        curElem.classList.add('card_match');
+    })
+}
+
+const resetValues = () => {
+    firstCard = "";
+    secondCard = "";
+    clickCount = 0;
+
+    let card_selected = document.querySelectorAll('.card_selected');
+
+    card_selected.forEach((curElem) => {
+        curElem.classList.remove('card_selected')
+    })
+
+}
+
+const parentDiv = document.querySelector('#card-section');
+
+parentDiv.addEventListener('click', (event) => {
+    let curCard = event.target;
+
+    if (curCard.id === "card-section")
+        return false;
+
+    clickCount++;
+
+    if (clickCount < 3) {
+
+        if (clickCount === 1) {
+            firstCard = curCard.parentNode.dataset.name;
+            firstCard_id = curCard.parentNode.dataset.id;
+            curCard.parentNode.classList.add('card_selected');
+        }
+        else {
+            if (curCard.parentNode.dataset.id == firstCard_id) {
+                clickCount--;
+                return false;
+            }
+            secondCard = curCard.parentNode.dataset.name;
+            curCard.parentNode.classList.add('card_selected');
+        }
+
+        if (firstCard !== "" && secondCard !== "") {
+            console.log(firstCard);
+            console.log(secondCard);
+            if (firstCard === secondCard) {
+                setTimeout(() => {
+                    card_matches();
+                    firstCard = "";
+                    secondCard = "";
+                    clickCount = 0;
+                }, 500)
+
+            } else {
+                setTimeout(() => {
+                    resetValues();
+                }, 500)
+            }
+        }
+    }
+})
+
+for (let i = 0; i < shuffledChild.length; i++) {
+    const childDiv = document.createElement('div');
+    // childDiv.style.backgroundImage = `url(${shuffledChild[i].img})`;
+
+    childDiv.classList.add('card');
+    childDiv.dataset.id = shuffledChild[i].id;
+    childDiv.dataset.name = shuffledChild[i].name;
+
+    const front_div = document.createElement('div');
+    front_div.classList.add('front-card')
+
+    const back_div = document.createElement('div');
+    back_div.classList.add('back-card')
+
+    back_div.style.backgroundImage = `url(${shuffledChild[i].img})`;
+
+    parentDiv.appendChild(childDiv)
+
+    childDiv.appendChild(front_div)
+    childDiv.appendChild(back_div)
+}
